486d8b701db1553304c9c15b198eae235a06fdf6
TorchAblaze <tiffanygreathead@gmail.com>
2021-03-24 10:17:06 -0700

    add finished files for working mad lib
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>
    co-authored-by: Brandon Magofna <Magofna68@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  $("#form1").submit(function (event) {
+    const person1Input = $("input#person1").val();
+    const person2Input = $("input#person2").val();
+    const adjective1Input = $("input#adjective1").val();
+    const noun1Input = $("input#noun1").val();
+    const person3Input = $("input#person3").val();
+    const quoteInput = $("input#quote1").val();
+    const pastTenseVerbInput = $("input#past-tense-verb").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".adjective1").text(adjective1Input);
+    $(".noun1").text(noun1Input);
+    $(".person3").text(person3Input);
+    $(".quote").text(quoteInput);
+    $(".past-tense-verb").text(pastTenseVerbInput);
+
+    $("story").show();
+
+    event.preventDefault();
+  });
+});
